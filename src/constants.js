131828eb3093c63b03787e12ef9731fcb9ec70b9
David Sandoval <davidsandoval281994@gmail.com>
2024-04-14 17:09:11 -0400

    initial commit

---
+++ src/constants.js
@@ -0,0 +1,29 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I mostly code in Python, C#, JavaScript and TypeScript these days,
+    but I've also worked with C, C++, Java, Rust, Golang, etc. Anyway regardless of the language, I just like programming and learning new things.
+    Here is my <a href="https://github.com/jsandoval6" target="_blank">Github</a>!`,
+  "cs-degree": `This is my CS degree. I graduated Summa Cum Laude from Utah Valley University!!
+   I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
+  "sofa-table": `That's my sofa. I like to relax here and watch Netflix or YouTube.
+    This is also a safe place to sleep when my wife gets mad at me 😂
+  `,
+  tv: `That's my TV. I've been watching a lot of One Piece lately and comedy shows! but I also like to watch tech Youtubers such as:
+   <a href="https://www.youtube.com/@NetworkChuck" target="_blank">NetworkChuck</a>, <a href="https://www.youtube.com/@davidbombal" target="_blank">David Bombal</a>,
+  <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@Fireship" target="_blank">Fireship</a>!`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. I try to relax before going to bed, otherwise my brain will think of everything and I won't be able to sleep much`,
+  resume: `This is my desk and on it is my resume. Check out my <a href="https://www.linkedin.com/in/david-sandoval-563212215/" target="_blank">LinkedIn</a>
+  and contact me for any interesting opportunities!`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.`,
+  library: `There are only a couple of books on my shelves, I move a lot and books are heavy!
+    I like to read a lot online. I've been reading a lot about: 
+     machine learning, 
+     ethical hacking, 
+     cybersecurity, 
+     programming,
+     etc (There is so much to learn and not much time!)
+  `,
+  exit: `leaving?, just close the tab. You might find me around a beach, gym, restaurant, theme park, or doing something adventurous! If you see me, come say hi! Bye 👋`,
+};
---
+++ src/main.js
@@ -0,0 +1,139 @@
+import { k } from "./kaboomCtx";
+import { scaleFactor, dialogueData } from "./constants";
+import { displayDialogue, setCamScale } from "./utils";
+
+k.loadSprite("spritesheet", "./spritesheet.png", {
+  sliceX: 39,
+  sliceY: 31,
+  anims: {
+    "idle-down": 936,
+    "walk-down": { from: 936, to: 939, loop: true, speed: 8 },
+    "idle-side": 975,
+    "walk-side": { from: 975, to: 978, loop: true, speed: 8 },
+    "idle-up": 1014,
+    "walk-up": { from: 1014, to: 1017, loop: true, speed: 8 },
+  },
+});
+
+k.loadSprite("map", "./map.png");
+
+k.setBackground(k.Color.fromHex("#311047"));
+
+k.scene("main", async () => {
+  const mapData = await (await fetch("./map.json")).json();
+  const layers = mapData.layers;
+
+  const map = k.add([k.sprite("map"), k.pos(0), k.scale(scaleFactor)]);
+
+  const player = k.make([
+    k.sprite("spritesheet", { anim: "idle-down" }),
+    k.area({
+      shape: new k.Rect(k.vec2(0, 3), 10, 10),
+    }),
+    k.body(),
+    k.anchor("center"),
+    k.pos(),
+    k.scale(scaleFactor),
+    {
+      speed: 250,
+      direction: "down",
+      isInDialog: false,
+    },
+    "player",
+  ]);
+
+  for (const layer of layers) {
+    if (layer.name === "boundaries") {
+      for (let boundary of layer.objects) {
+        map.add([
+          k.area({
+            shape: new k.Rect(k.vec2(0), boundary.width, boundary.height),
+          }),
+          k.body({ isStatic: true }),
+          k.pos(boundary.x, boundary.y),
+          boundary.name,
+        ]);
+
+        if (boundary.name) {
+          player.onCollide(boundary.name, () => {
+            player.isInDialog = true;
+            displayDialogue(
+              dialogueData[boundary.name],
+              () => (player.isInDialog = false)
+            );
+          });
+        }
+      }
+    }
+
+    if (layer.name === "spawnpoints") {
+      for (let entity of layer.objects) {
+        if (entity.name === "player") {
+          player.pos = k.vec2(
+            (map.pos.x + entity.x) * scaleFactor,
+            (map.pos.y + entity.y) * scaleFactor
+          );
+          k.add(player);
+        }
+      }
+    }
+  }
+
+  setCamScale(k);
+
+  k.onResize(() => {
+    setCamScale(k);
+  });
+
+  k.onUpdate(() => {
+    k.camPos(player.pos.x, player.pos.y + 100);
+  });
+
+  k.onMouseDown((mouseBtn) => {
+    if (mouseBtn !== "left" || player.isInDialog) return;
+
+    const worldMousePos = k.toWorld(k.mousePos());
+    player.moveTo(worldMousePos, player.speed);
+
+    const mouseAngle = player.pos.angle(worldMousePos);
+
+    const lowerBound = 50;
+    const upperBound = 125;
+
+    if (
+      mouseAngle > lowerBound &&
+      mouseAngle < upperBound &&
+      player.curAnim !== "walk-up"
+    ) {
+      player.play("walk-up");
+      player.direction = "up";
+    } else if (
+      mouseAngle < -lowerBound &&
+      mouseAngle > -upperBound &&
+      player.curAnim !== "walk-down"
+    ) {
+      player.play("walk-down");
+      player.direction = "down";
+    } else if (Math.abs(mouseAngle) > upperBound) {
+      player.flipX = false;
+      if (player.curAnim() !== "walk-side") player.play("walk-side");
+      player.direction = "left";
+    } else if (Math.abs(mouseAngle) < lowerBound) {
+      player.flipX = true;
+      if (player.curAnim() !== "walk-side") player.play("walk-side");
+      player.direction = "right";
+    }
+  });
+
+  k.onMouseRelease(() => {
+    if (player.direction === "down") {
+      player.play("idle-down");
+    } else if (player.direction === "up") {
+      player.play("idle-up");
+    } else {
+      player.play("idle-side");
+    }
+  });
+});
+
+k.go("main");
